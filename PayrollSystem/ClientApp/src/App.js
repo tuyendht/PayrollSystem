040432ae85ab789dc47a8a97b52cf0a4cd7b32ac
tqhhuy <huy95huy95@gmail.com>
2021-10-01 09:46:54 +0700

    login

--- PayrollSystem/ClientApp/src/App.js
+++ PayrollSystem/ClientApp/src/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 import { Route } from 'react-router';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Switch, Link } from "react-router-dom";
-import Login from "./components/Loginform/login.component";
+import Login from "./components/Loginform/login";
 import Layout from "./components/Layout/layout.js";
 import Home from "./views/home.js"
 import Taxes from "./views/taxes.js"
-
 export default class App extends Component {
   static displayName = App.name;
-
   render () {
       return (
         <Router>
@@ -28,8 +26,106 @@ export default class App extends Component {
                         </Switch>
                     </div>
                 </div>
-            </div> */}
+            </div> 
+            }
         </Router>
       );
   }
+*/
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+export default function App() {
+    const classes = useStyles();
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    const [user, setUser] = useState(null);
+
+
+
+    useEffect(() => {
+        // FIR Auth State Observer
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                var uid = user.uid;
+                console.log(`User has signed in with UID: ${uid}`)
+                setUser(uid)
+
+            } else {
+                // User is signed out
+                console.log('User is not signed in.')
+                setUser(null)
+            }
+        });
+    }, [])
+
+    function signInWithGooglePopUp() {
+        firebase.auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                console.log('User has signed in.')
+            }).catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                console.log(`Errors occurred during sign in: ${errorCode}, ${errorMessage}, ${email}, ${credential}`)
+            });
+
+    }
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title}>
+                        Firebase Demo
+                    </Typography>
+                    {user ? (
+                        <div style={{ display: "flex", alignItems: 'center' }}>
+                            <Typography variant="body1">
+                                {user}
+                            </Typography>
+                            <Button color="inherit"
+                                onClick={() => {
+                                    // signout
+                                    firebase.auth().signOut()
+                                        .then(() => {
+                                            console.log('User has signed out.')
+                                        })
+                                        .catch((err) => {
+                                            console.log('Error signing out: ', err)
+                                        })
+                                }}>Signout</Button>
+                        </div>
+                    ) : (
+                        <Button color="inherit"
+                            onClick={() => {
+                                signInWithGooglePopUp()
+                            }}>Login</Button>
+                    )}
+
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
--- PayrollSystem/ClientApp/src/index.js
+++ PayrollSystem/ClientApp/src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from "react-router-dom";
-
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 ReactDOM.render(
@@ -11,4 +10,39 @@ ReactDOM.render(
         <App />
     </BrowserRouter>,
     rootElement);
+registerServiceWorker();
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from "react-router-dom";
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC373L24Sdhih-2oDikayTgVT5heMNoQ0U",
+    authDomain: "payroll-9642e.firebaseapp.com",
+    projectId: "payroll-9642e",
+    storageBucket: "payroll-9642e.appspot.com",
+    messagingSenderId: "635041237967",
+    appId: "1:635041237967:web:743052b216c15d747b60ba",
+    measurementId: "G-DV8QKF9YVS"
+};
+
+if (!firebase.apps[0]) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 registerServiceWorker();
