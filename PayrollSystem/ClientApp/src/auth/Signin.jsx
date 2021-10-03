72fd5b4c3d9c81a759a8e91022721cf35aaf78db
tqhhuy <huy95huy95@gmail.com>
2021-10-04 01:28:34 +0700

    [huytq] authentication

---
+++ PayrollSystem/ClientApp/src/auth/Signin.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+// import FirebaseAuth from './FirebaseAuth';
+
+SignIn.propTypes = {
+
+};
+  // Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'redirect',
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: '/SignIn',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+};
+
+function SignIn() {
+  return (
+    <div>
+      <div className="text-center">
+        <h2>Login With: </h2>
+
+        
+      </div>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </div>
+  );
+}
+
+export default SignIn;
